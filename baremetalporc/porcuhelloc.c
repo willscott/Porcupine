049cc6648a221cf0589829bf8c67c7cb2b6213c4
ryscheng <ryscheng@office.(none)>
2011-10-25 13:54:09 -0700

    Changes in progress to add Baremetal support. Don't use this!

---
+++ baremetalporc/porcuhelloc.c
@@ -0,0 +1,7 @@
+#include "libBareMetal.h"
+
+int main(void) {
+  b_print_string("Hello World! says the Porc \n");
+  return 0;
+
+}
---
+++ baremetalporc/porcupine.c
@@ -0,0 +1,8 @@
+#include "libBareMetal.h"
+#include "porcubaby.c"
+
+int main(void) {
+  b_print_string("Hello World! says the Porc \n");
+  return 0;
+
+}
