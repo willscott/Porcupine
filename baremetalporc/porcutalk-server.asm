f875e4c227a30fee7b79a788b581b579bd1fbe08
Will Scott <willscott@gmail.com>
2011-11-10 14:28:27 -0800

    added smp.

--- baremetalporc/porcutalk-server.asm
+++ baremetalporc/porcutalk-server.asm
@@ -4,27 +4,41 @@
 %include "bmdev.asm"
 
 start:
-	call b_smp_numcores
 	rdtsc			;Time = 64-bit stored in EDX:EAX
 	mov ebx, [cycles_in_epoch]
 	div ebx			;Divides EDX:EAX by EBX and stores in EDX:EAX
 	mov [current_epoch], eax
+	call b_smp_numcores ; RAX gets number of cores.
+	mov rcx, rax        ; move number to ecx
+  mov rax, pad_nop    ; set smp workload pointer
+
+spawn:
+  call b_smp_enqueue
+  sub rcx, 1
+  cmp rcx, 0
+  jne spawn
 
 pad_nop:
-	rdtsc			;Time = 64-bit stored in EDX:EAX
-	mov rcx, rax		;Save lower 32-bits of time for start_loop
+  call b_smp_get_id
+  mov r8, rax;
+  mov rcx,16;
+  mul rcx;
+  mov r9, rax;
+
+	rdtsc			     ;Time = 64-bit stored in EDX:EAX
+	mov rcx, rax	 ;Save lower 32-bits of time for start_loop
 	mov ebx, [cycles_in_epoch];Calculate epoch number
 	div ebx			;Divides EDX:EAX by EBX and stores in EDX:EAX
 
-	cmp eax, [current_epoch];Detect transition to next epoch
+	cmp eax, [current_epoch + 4*r8];Detect transition to next epoch
 	je pad_nop		;If not yet, pad some more
-	mov [current_epoch], eax;Update current epoch
+	mov [current_epoch + 4*r8], eax;Update current epoch
 
 start_loop:
 	rdtsc
 	shl rdx, 32		;Calculate start time, store in rcx
 	xor rax, rdx
-	mov [start_time], rax	;Store the start time
+	mov [start_time + r9], rax	;Store the start time
 	xor rcx, rcx		;Clear rcx
 	mov ecx, [num_loops]
 
@@ -37,7 +51,7 @@ end_loop:
 	rdtsc			;Print time it took
 	shl rdx, 32
 	xor rax, rdx
-	mov rdx, [start_time]
+	mov rdx, [start_time + r9]
 	sub rax, rdx
 	mov rdi, output_str	;string parameter for b_int_to_string
 	mov rsi, rdi 		;parameter for b_print_string
@@ -48,7 +62,7 @@ end_loop:
 	call b_print_string
 
 	xor rax, rax		;DEBUG Print Current Epoch
-	mov eax, [current_epoch]
+	mov eax, [current_epoch + 4*r8]
 	mov rdi, output_str	;string parameter for b_int_to_string
 	mov rsi, rdi 		;parameter for b_print_string
 	call b_int_to_string
@@ -67,13 +81,15 @@ exit:
 	
 ret
 
-output_str: 	times 21 db 0	;Space for printing time
-cr_str: 	db 13, 0
-dash_str:	db 32, 45, 32, 0
+section .data
+  output_str: 	times 21 db 0	;Space for printing time
+  cr_str: 	db 13, 0
+  dash_str:	db 32, 45, 32, 0
 
-cycles_in_epoch:dd 0xCAA7E200	;3.4*10^9 cycles = 1 second
-num_loops: 	dd 0x00000800	;Number of loops to run per epoch
-num_epochs:	dd 0x0000000a	;Number of epochs to run in program
+  cycles_in_epoch:dd 0xCAA7E200	;3.4*10^9 cycles = 1 second
+  num_loops: 	dd 0x00000800	;Number of loops to run per epoch
+  num_epochs:	dd 0x0000000a	;Number of epochs to run in program
 
-current_epoch:	dd 0x00000000	;Current epoch number
-start_time:	dq 0x0000000000000000
+section .bss
+  current_epoch: resd 8; Current epoch number
+  start_time:    resq 8;
